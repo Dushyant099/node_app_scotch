4a25a4ffba18c79f2b37c2360dbc778cea931874
Dushyant Patel <dushyant@C3-PO.local>
2017-01-12 22:59:43 -0500

    add stling on index page

--- server.js
+++ server.js
@@ -32,6 +32,11 @@
  //route our app
  var router = require('./app/routes');
  app.use('/',router);
+
+//set static files like css, img
+
+app.use(express.static(__dirname + '/public'))
+
 //start the server
 app.listen(port,function() {
 	console.log('app started');
